b40eedbebd202688efdfa9963cf5c7825fac093c
shaik wazid <shaikwazid2705@gmailcom>
2023-11-08 15:10:39 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+let String = "";
+
+let buttons = document.querySelectorAll("#button");
+
+Array.from(buttons).forEach((button)=>{
+    button.addEventListener('click',(e)=>{
+        if(e.target.innerHTML == '='){
+            String = eval(String);
+            document.querySelector(".input").value = String;
+        }else if(e.target.innerHTML == 'AC'){
+            String = "";
+            document.querySelector(".input").value = String;
+        }else{
+            console.log(e.target);
+            String = String+e.target.innerHTML;
+            document.querySelector('input').value = String;
+        }
+    })
+})
